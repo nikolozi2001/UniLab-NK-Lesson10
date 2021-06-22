d0bacc81294a23bf00a68ce1ae55b98975cbdba3
nikolozi2001 <34250988+nikolozi2001@users.noreply.github.com>
2021-06-23 00:40:19 +0400

    update database

--- contact.php
+++ contact.php
@@ -8,7 +8,7 @@ $email = $_POST['email'];
 
 //Database connection
 
-$conn = new mysqli('localhost', 'root', 'contact');
+$conn = new mysqli('localhost', 'root', "", 'contact');
 if($conn->connect_error){
     echo "$conn->connect_error";
     die("Connection Failed : ". $conn->connect_error);
